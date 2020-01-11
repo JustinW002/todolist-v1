762e57be609be588bbb31ec8bdf2f58299f6d802
Justin Woods <justinwoods002@gmail.com>
2020-01-11 13:33:22 -0700

    make date.js module and refacto app.js to use the module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require('ejs');
+const date=require(__dirname+"/date.js");
 const app=express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -10,17 +11,11 @@ app.listen(3000,function(){
     console.log("connected to port 3000");
 });
 
-let items=["buy food", "cook food", "eat food"];
-let workItems=[];
+const items=["buy food", "cook food", "eat food"];
+const workItems=[];
 
 app.get("/",function(req,res){
-    let today= new Date();
-    let options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-    let day=today.toLocaleDateString("en-US",options);
+    let day=date.getDate();
     res.render("list",{listTitle:day, newListItems:items});
 });
 
