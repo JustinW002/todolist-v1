e564da1c6cb9c575e6529622208a1f4030cf5fa1
Justin Woods <justinwoods002@gmail.com>
2020-01-11 11:21:19 -0700

    logic to output day of week to template

--- app.js
+++ app.js
@@ -13,16 +13,24 @@ app.listen(3000,function(){
 app.get("/",function(req,res){
     var today= new Date();
 var currentDay= today.getDay();
-var day="";
-
-    if(currentDay === 6 || currentDay === 0){
-       day="Weekend";
-       res.render("list",{kindOfDay:day});
-    }else{
-        day="Weekday";
-       res.render("list",{kindOfDay:day});
-    }
+var day=selectDay(currentDay); 
+    res.render("list",{kindOfDay:day});
 });
+
+function selectDay(number){
+    switch (number) {
+        case 0: return "Sunday"
+        case 1: return "Monday"
+        case 2: return "Tuesday"
+        case 3: return "Wednesday"
+        case 4: return "Thursday"
+        case 5: return "Friday"
+        case 6: return "Saturday"
+        default:
+            console.log("incorrect date");
+            return "Not a day of the week";
+    }
+}
 
 
 
